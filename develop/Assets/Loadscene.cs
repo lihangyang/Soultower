453f9df71b0d580cc735d5abee147b23f5fd247b
Leif <leif@LeifdeMacBook-Pro.local>
2018-06-08 14:34:51 +0800

    提交改动

---
+++ develop/Assets/Loadscene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loadscene : MonoBehaviour {
+
+    public string NextLvscene;
+    //public string NextLvname;
+	// Use this for initialization
+	void Start () {
+      
+	}
+
+    // Update is called once per frame
+
+    public void Client()
+    {
+        //Debug.Log("jianting");
+        SceneManager.LoadScene(NextLvscene);
+    }
+}
